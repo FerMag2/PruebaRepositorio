b3195cf336e30fcc6ae96cbdedf0213797f5f8ec
FerMag2 <73145422+FerMag2@users.noreply.github.com>
2021-03-03 17:36:37 -0600

    Hola Funciono

---
+++ FernandoMagdielChiCanul/Ejercicio1.java
@@ -0,0 +1,15 @@
+package FernandoMagdielChiCanul;
+import java.util.Scanner;
+public class Ejercicio1 {
+        public static void main(String[]args) {
+        int a,b;
+        Scanner S= new Scanner(System.in);
+        System.out.println("Ingresa un numero");
+        a=S.nextInt();
+        System.out.println("Ingresa un numero");
+        b=S.nextInt();
+        System.out.println("La suma es "+ (a+b));
+        
+        }
+    
+}
